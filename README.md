6a568c80b7345bc1163d1e95ab11398d0851847f
Callum-OP <callumoprey@gmail.com>
2025-07-22 16:37:01 +0100

    Redesigned app as a web app, changed code into javascript and added html

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # How to run
-Replace the "LineDrawing.png" file with whatever image you want (keeping the same name - "LineDrawing.png"), then simply run the file as you would any python file, eg
-  python -u "main.py"
-Although make sure your in the same directory as the file
+Visit the website, enter a png image and it will
 
 # About the program
-I wanted a program that would apply anti-aliasing to an existing digital line drawing that didn't have anti-aliasing, so I created this simple Python program.
+I wanted a program that would apply anti-aliasing to an existing digital line drawing that didn't have anti-aliasing, so I created this simple web app.
 
-By placing and naming a file "LineDrawing.png" and running the program, it will be modified to have an anti-aliasing effect and the modified version will placed in "output.png". It does not matter if the line drawing is transparent or not as this program will automatically make the final image transparent by removing white space.
+By placing a file in the site, it will be modified to have an anti-aliasing effect with the option to download or copy to clipboard.
 
 This program does not work for coloured images and is only designed for line art.
 
-This program works by thinning the lines in the image and then blending it with itself to create an anti-aliasing effect, it then slightly blurs and darkens the blended result and then overlays it with a 1 pixel line version of the image to ensure there is always at least a 1 pixel line remaning. The image will have any white space removed from it and the semi-transparent pixels will be darkened. The final image is then overlayed over itself to create a stronger/darker line drawing. The result is the final image will likely look more smooth and slightly thinner as if it was drawn with a brush with anti-aliasing as well as be transparent so it is ready to be used.
+This program works by blurring the image, thinning the lines in the image and fading the edges to create an anti-aliasing effect. The result is the final image will likely look more smooth and slightly thinner as if it was drawn with a brush with anti-aliasing, the image should also remain transparent and ready to be used.
 
 
 Below is a before and after image.
-
-<img width="569" alt="Anti-Aliaser" src="https://github.com/user-attachments/assets/5d64b860-0849-4bc8-8ae8-d0ec4f6307d6" />
---
+++ script.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const upload = document.getElementById("upload");
+
+// --- Load image ---
+// Wait for image to be entered and read and process the image
+upload.addEventListener("change", function () {
+    // Read file
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const img = new Image();
+        // Once image is loaded
+        img.onload = function () {
+            // Resize html canvas to match image
+            canvas.width = img.width;
+            canvas.height = img.height;
+            // Apply a slight blur
+            ctx.filter = 'blur(0.6px)';
+            ctx.drawImage(img, 0, 0);
+            ctx.filter = 'none';
+            // Call function
+            processCanvas();
+        };
+        img.src = e.target.result;
+    };
+    reader.readAsDataURL(upload.files[0]);
+});
+
+// --- Process image ---
+function processCanvas() {
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    // Loop through pixel data
+    for (let i = 0; i < data.length; i += 4) {
+        // Extract pixel data
+        const r = data[i], g = data[i + 1], b = data[i + 2];
+
+        // Calculate brightness
+        const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
+        // Fade pixels at edges
+        const alphaRatio = Math.pow((255 - brightness) / 255, 2.8);
+        const alpha = Math.floor(alphaRatio * 255);
+
+        // Soften lines
+        data[i] = data[i + 1] = data[i + 2] = 0;
+        data[i + 3] = alpha;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    // Set up option to download
+    canvas.toBlob(blob => { // Convert canvas to a Blob
+        const download = document.getElementById("download");
+        download.href = URL.createObjectURL(blob);
+        download.download = "anti-aliased.png";
+        download.style.display = "inline";
+    }, "image/png");
+}
+
+// Set up option to copy to clipboard
+document.getElementById("copy").addEventListener("click", () => { // Wait for click
+    canvas.toBlob(blob => { // Convert canvas to a Blob
+        const item = new ClipboardItem({ "image/png": blob });
+        navigator.clipboard.write([item])
+            .then(() => alert("Copied!"))
+            .catch(err => console.error("Clipboard failed:", err));
+    });
+});
